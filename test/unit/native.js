4b92ae717ddeb8373e165bc7dbd3297067e7134f
Victor Queiroz <victorcqueirozg@gmail.com>
2015-05-14 08:08:31 -0300

    collection bug  fixed

--- test/unit/native.js
+++ test/unit/native.js
@@ -7,8 +7,7 @@ describe('native', function () {
 		Native.connect('mongodb://localhost/mydbtest').then(function (db) {
 			assert.ok(db instanceof MongoNative.Db);
 			return db.createCollection('my_first_collection').then(function (collection) {
-				console.log(collection)
-				// return collection.insertMany([{a: 1}, {b: 2}, {a: 3}]);
+				return collection.insertMany([{a: 1}, {b: 2}, {a: 3}]);
 			}).then(function (docs) {
 				assert.equal(3, docs.length)
 				done()
